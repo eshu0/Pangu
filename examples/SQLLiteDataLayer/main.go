1d2836379bad9ed2a14a94c989abc6c809860b4b
eshu0 <willsawesometoolkit@gmail.com>
2020-12-10 19:53:32 +0000

    Update main.go

--- examples/SQLLiteDataLayer/main.go
+++ examples/SQLLiteDataLayer/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"os"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"path/filepath"	
-	"text/template"
+	"os"
+	"path/filepath"
 	"strings"
-	sl "github.com/eshu0/simplelogger"
-	sli "github.com/eshu0/simplelogger/interfaces"
+	"text/template"
+
 	anl "github.com/eshu0/pangu/pkg/analysers"
+	sl "github.com/eshu0/simplelogger/pkg"
+	sli "github.com/eshu0/simplelogger/pkg/interfaces"
 )
 
 func main() {
@@ -38,32 +39,32 @@ func main() {
 			}
 			if !info.IsDir() && filepath.Ext(path) == ".db" {
 				fmt.Printf("Parsing database: %+v \n", info.Name())
-				Parse(path, outputdir,slog)
+				Parse(path, outputdir, slog)
 				return nil
 			}
 			fmt.Printf("visited file or dir: %q\n", path)
 			return nil
 		})
-	}else{
+	} else {
 		Parse(*dbname, outputdir, slog)
 	}
 }
 
-func CheckCreatePath(slog sli.ISimpleLogger, path string, panicif bool){
+func CheckCreatePath(slog sli.ISimpleLogger, path string, panicif bool) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		if panicif { 
+		if panicif {
 			panic(path + " not found!")
-		}else{
+		} else {
 			os.Mkdir(path, 0777)
-			fmt.Println("Created: "+path)	
+			fmt.Println("Created: " + path)
 		}
-	
-	}else{
-		fmt.Println("Exists: "+path)	
+
+	} else {
+		fmt.Println("Exists: " + path)
 	}
 }
 
-func CreateAndExecute(slog sli.ISimpleLogger, filename string, templ *template.Template, data interface{}){
+func CreateAndExecute(slog sli.ISimpleLogger, filename string, templ *template.Template, data interface{}) {
 	file, err := os.Create(filename)
 	if err != nil {
 		slog.LogError("CreateAndExecute", fmt.Sprintf("Cannot create file%s", err.Error()))
@@ -78,13 +79,12 @@ func CreateAndExecute(slog sli.ISimpleLogger, filename string, templ *template.T
 	file.Close()
 }
 
-func Parse(dbname string, odir string, slog sli.ISimpleLogger){
+func Parse(dbname string, odir string, slog sli.ISimpleLogger) {
 
-	dbfolder := strings.Replace(filepath.Base(dbname),filepath.Ext(dbname),"",-1)
-
+	dbfolder := strings.Replace(filepath.Base(dbname), filepath.Ext(dbname), "", -1)
 
 	outputdir := odir + strings.Title(dbfolder)
-	fmt.Println("Outputting to: "+outputdir)
+	fmt.Println("Outputting to: " + outputdir)
 
 	datastoredir := outputdir + "/Datastore/"
 	handlerdir := outputdir + "/Handlers/"
@@ -93,7 +93,6 @@ func Parse(dbname string, odir string, slog sli.ISimpleLogger){
 	restdir := outputdir + "/REST/"
 	controllersdir := restdir + "/Controllers/"
 
-	
 	CheckCreatePath(slog, dbname, true)
 	CheckCreatePath(slog, odir, false)
 	CheckCreatePath(slog, outputdir, false)
@@ -104,14 +103,13 @@ func Parse(dbname string, odir string, slog sli.ISimpleLogger){
 	CheckCreatePath(slog, controllersdir, false)
 	CheckCreatePath(slog, restdir, false)
 
-
 	fds := &anl.DatabaseAnalyser{}
 	fds.Filename = dbname
 	fds.Create(slog)
 
 	dbstruct := fds.GetDatabaseStructure()
 
-	CodeTemplate := CreateTemplate("./Templates/CodeTemplate.txt","code")
+	CodeTemplate := CreateTemplate("./Templates/CodeTemplate.txt", "code")
 	DataTemplate := CreateTemplate("./Templates/DataTemplate.txt", "data")
 	DLTemplate := CreateTemplate("./Templates/DLTemplate.txt", "dl")
 	MainTemplate := CreateTemplate("./Templates/MainTemplate.txt", "main")
@@ -119,29 +117,29 @@ func Parse(dbname string, odir string, slog sli.ISimpleLogger){
 	RESTServerTemplate := CreateTemplate("./Templates/RESTServer.txt", "control")
 
 	// Execute the template for each recipient.
-	ctemplates := GenerateFile(dbstruct,slog)
+	ctemplates := GenerateFile(dbstruct, slog)
 
-	for _, cs := range ctemplates {	
-		CreateAndExecute(slog, handlerdir+cs.GetHandlersName()+ ".go", CodeTemplate, cs)
-		CreateAndExecute(slog, controllersdir+cs.GetHandlersName()+ ".go", ControllersTemplate, cs)
-		CreateAndExecute(slog, modelsdir+cs.GetDataName() + ".go", DataTemplate, cs)
-	}	
+	for _, cs := range ctemplates {
+		CreateAndExecute(slog, handlerdir+cs.GetHandlersName()+".go", CodeTemplate, cs)
+		CreateAndExecute(slog, controllersdir+cs.GetHandlersName()+".go", ControllersTemplate, cs)
+		CreateAndExecute(slog, modelsdir+cs.GetDataName()+".go", DataTemplate, cs)
+	}
 
-	dl := Datats{Database: ctemplates[0].Database, Templates:ctemplates}
-	
-	CreateAndExecute(slog, datastoredir+dl.Database.FilenameTrimmed+".go",DLTemplate, dl)
-	CreateAndExecute(slog, appdir+"main.go",MainTemplate, ctemplates)
-	CreateAndExecute(slog, restdir+"main.go",RESTServerTemplate, ctemplates)
+	dl := Datats{Database: ctemplates[0].Database, Templates: ctemplates}
+
+	CreateAndExecute(slog, datastoredir+dl.Database.FilenameTrimmed+".go", DLTemplate, dl)
+	CreateAndExecute(slog, appdir+"main.go", MainTemplate, ctemplates)
+	CreateAndExecute(slog, restdir+"main.go", RESTServerTemplate, ctemplates)
 
 }
 
 func CreateTemplate(filepath string, name string) *template.Template {
 	b1, err1 := ioutil.ReadFile(filepath) // just pass the file name
-    if err1 != nil {
+	if err1 != nil {
 		fmt.Print(err1)
 		return nil
-    }
-    str1 := string(b1) // convert content to a 'string'
+	}
+	str1 := string(b1) // convert content to a 'string'
 
 	// Create a new template and parse the letter into it.
 	return template.Must(template.New(name).Parse(str1))
