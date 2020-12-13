47c3cc5521a1ab5f8c4d7d62aa9ada0a378121a7
eshu0 <willsawesometoolkit@gmail.com>
2020-12-13 07:21:04 +0000

    Update Todos.go

--- examples/Autogen/Todos/Datastore/Todos.go
+++ examples/Autogen/Todos/Datastore/Todos.go
@@ -1,12 +1,12 @@
 package pgudatastore
 
 import (
-	SQLL "github.com/eshu0/persist/pkg/sqllite"
 	sli "github.com/eshu0/simplelogger/pkg/interfaces"
-
+	SQLL "github.com/eshu0/persist/pkg/sqllite"
 	//per "github.com/eshu0/persist/pkg/interfaces"
 	hndlr "github.com/eshu0/Pangu/examples/Autogen/Todos/Handlers"
 )
+
 
 type TodosDatastore struct {
 	Datastore *SQLL.SQLLiteDatastore
@@ -24,36 +24,40 @@ type TodosDatastore struct {
 	//
 }
 
-func CreateDataStorage(log sli.ISimpleLogger, filename string) *TodosDatastore {
-	ds := SQLL.CreateOpenSQLLiteDatastore(log, filename)
+func CreateDataStorage(filename string) *TodosDatastore {
+	res := TodosDatastore{}
 
+	ds := SQLL.CreateOpenSQLLiteDatastore(filename)
+	
 	// tests the example
-	ds.SetStorageHander("Generic", SQLL.NewSQLLiteTableHandler(ds))
+	ds.SetStorageHander("Generic",SQLL.NewSQLLiteTableHandler(ds)) 
+	
+	ds.SetStorageHander("Projects",hndlr.NewProjectsHandler(ds))
+	
+	ds.SetStorageHander("Jobs",hndlr.NewJobsHandler(ds))
+	
+	ds.SetStorageHander("Tasks",hndlr.NewTasksHandler(ds))
+	
+	ds.SetStorageHander("JobHasTasks",hndlr.NewJobHasTasksHandler(ds))
+	
+	ds.SetStorageHander("ProjectHasJobs",hndlr.NewProjectHasJobsHandler(ds))
+	
 
-	ds.SetStorageHander("Projects", hndlr.NewProjectsHandler(ds))
-
-	ds.SetStorageHander("Jobs", hndlr.NewJobsHandler(ds))
-
-	ds.SetStorageHander("Tasks", hndlr.NewTasksHandler(ds))
-
-	ds.SetStorageHander("JobHasTasks", hndlr.NewJobHasTasksHandler(ds))
-
-	ds.SetStorageHander("ProjectHasJobs", hndlr.NewProjectHasJobsHandler(ds))
 
 	ds.CreateStructures()
 
-	res := TodosDatastore{}
 	res.Datastore = ds
-
+	
 	return &res
 }
+
 
 func (fds *TodosDatastore) GetProjectsHandler() *hndlr.ProjectsHandler {
 
 	data, ok := fds.Datastore.GetStorageHandler("Projects")
 	if ok {
-		res := data.(*hndlr.ProjectsHandler)
-		return res
+	  res := data.(*hndlr.ProjectsHandler)
+	  return res
 	}
 	return nil
 }
@@ -62,8 +66,8 @@ func (fds *TodosDatastore) GetJobsHandler() *hndlr.JobsHandler {
 
 	data, ok := fds.Datastore.GetStorageHandler("Jobs")
 	if ok {
-		res := data.(*hndlr.JobsHandler)
-		return res
+	  res := data.(*hndlr.JobsHandler)
+	  return res
 	}
 	return nil
 }
@@ -72,8 +76,8 @@ func (fds *TodosDatastore) GetTasksHandler() *hndlr.TasksHandler {
 
 	data, ok := fds.Datastore.GetStorageHandler("Tasks")
 	if ok {
-		res := data.(*hndlr.TasksHandler)
-		return res
+	  res := data.(*hndlr.TasksHandler)
+	  return res
 	}
 	return nil
 }
@@ -82,8 +86,8 @@ func (fds *TodosDatastore) GetJobHasTasksHandler() *hndlr.JobHasTasksHandler {
 
 	data, ok := fds.Datastore.GetStorageHandler("JobHasTasks")
 	if ok {
-		res := data.(*hndlr.JobHasTasksHandler)
-		return res
+	  res := data.(*hndlr.JobHasTasksHandler)
+	  return res
 	}
 	return nil
 }
@@ -92,8 +96,11 @@ func (fds *TodosDatastore) GetProjectHasJobsHandler() *hndlr.ProjectHasJobsHandl
 
 	data, ok := fds.Datastore.GetStorageHandler("ProjectHasJobs")
 	if ok {
-		res := data.(*hndlr.ProjectHasJobsHandler)
-		return res
+	  res := data.(*hndlr.ProjectHasJobsHandler)
+	  return res
 	}
 	return nil
 }
+
+
+
