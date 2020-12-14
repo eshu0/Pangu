7e28f3c70d50d4440a885358f795df987c663df1
eshu0 <willsawesometoolkit@gmail.com>
2020-12-14 00:04:54 +0000

    Update models.go

--- pkg/generator/models.go
+++ pkg/generator/models.go
@@ -12,7 +12,7 @@ type Model struct {
 }
 
 func (cg *Model) GetFileName() string {
-	name := strings.ToLower(cs.Table.Name)
+	name := strings.ToLower(cg.Table.Name)
 
 	// crude way to take items and make it singular
 	if last := len(name) - 1; last >= 0 && name[last] == 's' {
